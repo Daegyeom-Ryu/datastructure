1c073dc18e057b75e02283368f345604edb144b0
daegyeom <hyunbin9341@gmail.com>
2023-09-14 20:23:50 +0900

    singlylinkedlist unshift,shift and test code

--- singlylinkedlist.js
+++ singlylinkedlist.js
@@ -12,7 +12,7 @@ class SinglyLinkedList {
     }
     push(value) {   // 맨 뒤에 삽입 후 리스트 반환
         let newNode = new Node(value);
-        if(!this.head) {
+        if(!this.head) {    
             this.head = newNode;
             this.tail = newNode;
         } else {
@@ -41,17 +41,43 @@ class SinglyLinkedList {
         this.length--;
         return poppedNode;
     }
+    unshift(value) {    // 맨 앞에 삽입 후 리스트 반환
+        let newNode = new Node(value);
+        if(!this.head) {    
+            this.head = newNode;
+            this.tail = newNode;
+        } else {    
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    shift() {   // 맨 앞에서 삭제 후 삭제한 노드 반환
+        if(!this.head)  return undefined;
+        let oldHead = this.head;
+        if(this.head === this.tail) {   // 노드 1개
+            this.head = null;
+            this.tail = null;
+        } else {    // 노드 2개 이상
+            this.head = oldHead.next;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
+    
 }
 let list = new SinglyLinkedList();
-list.push(3);
-list.push(2);
-list.push(1);
+list.unshift(1);
+list.unshift(2);
+list.unshift(3);
 console.log(list);
-list.pop();
+console.log(list.shift());
 console.log(list);
-list.pop();
+console.log(list.shift());
 console.log(list);
-list.pop();
+console.log(list.shift());
 console.log(list);
-list.pop();
+console.log(list.shift());
 console.log(list);
