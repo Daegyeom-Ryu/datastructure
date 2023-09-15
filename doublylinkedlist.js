cba01b326483c9be1027aa67e98cc7f593175a24
daegyeom <hyunbin9341@gmail.com>
2023-09-15 16:40:49 +0900

    doublylinkedlist unshift,shift and test code

--- doublylinkedlist.js
+++ doublylinkedlist.js
@@ -39,6 +39,34 @@ class DoublyLinkedList {
         this.length--;
         return poppedNode;
     }
+    unshift(value) {
+        let newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    shift() {
+        if(!this.head)  return undefined;
+        let oldHead = this.head;
+        if(this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            let newHead = this.head.next;
+            newHead.prev = null;
+            this.head = newHead;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
     print() {
         let current = this.head;
         let arr = [];
@@ -50,19 +78,35 @@ class DoublyLinkedList {
         return arr;
     }
 }
-// push, pop test code
+// unshift, shift test code
 let list = new DoublyLinkedList();
-
-list.push(1);
-list.push(2);
-list.push(3);
-list.push(4);
-list.push(5);
+console.log('unshift, shift test code');
+list.unshift(5);
+list.unshift(4);
+list.unshift(3);
+list.unshift(2);
+list.unshift(1);
 list.print(); // [1,2,3,4,5]
-console.log(list.pop()) // 5 prev=null, next=null 
-console.log(list.pop()) // 4
-console.log(list.pop()) // 3
-list.print(); // [1,2]
-console.log(list.pop()) // 2
-console.log(list.pop()) // 1
-console.log(list.pop()) // undefined
+console.log(list.shift()); // 1 prev = null, next = null
+console.log(list.shift()); // 2
+console.log(list.shift()); // 3
+list.print(); // [4,5]
+console.log(list.shift()); // 4
+console.log(list.shift()); // 5
+console.log(list.shift()); // undefined
+
+// push, pop, unshift, shift test code
+console.log('push, pop, unshift, shift test code');
+list.push(4);
+list.unshift(3);
+list.unshift(2);
+list.push(5);
+list.unshift(1);
+list.print(); // [1,2,3,4,5]
+
+console.log(list.pop()); // 5
+console.log(list.shift()); // 1 
+console.log(list.pop()); // 4
+console.log(list.shift()); // 2  
+console.log(list.pop()); // 3
+console.log(list.pop()); // undefined; 
