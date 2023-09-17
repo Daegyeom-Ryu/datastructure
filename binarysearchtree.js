174960f8d92836a79fe6aa1b41cafaa91b350c68
daegyeom <hyunbin9341@gmail.com>
2023-09-17 18:45:38 +0900

    DFS pre,in,postOrder,BFS and test code

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -1,8 +1,39 @@
-class Node {
+class Node {    // tree node
     constructor(value) {
         this.value = value;
         this.left = null;
         this.right = null;
+    }
+}
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    enqueue(treeNode) { 
+        if(!this.first) {
+            this.first = treeNode;
+            this.last = treeNode;
+        } else {
+            this.last.next = treeNode;
+            this.last = treeNode;
+        }
+        this.size++;
+        return this;
+    }
+    dequeue() {
+        if(!this.first) return undefined;
+        let oldFirst = this.first;
+        if(this.first === this.last) {
+            this.first = null;
+            this.last = null;
+        } else {
+            this.first = oldFirst.next;
+            delete oldFirst.next;   // enqueue에서 node에 추가된 next property 제거
+        }
+        this.size--;
+        return oldFirst;
     }
 }
 class BinarySearchTree {
@@ -49,29 +80,75 @@ class BinarySearchTree {
         if(!found)  return undefined;
         return currNode;
     }
+    DFSPreOrder() {
+        let visited = [];
+        function preOrder(currNode) {
+            if(!currNode)  return;
+            visited.push(currNode.value);
+            if(currNode.left)   preOrder(currNode.left);
+            if(currNode.right)  preOrder(currNode.right);
+        }
+        preOrder(this.root);
+        return visited;
+    }
+    DFSInOrder() {
+        let visited = [];
+        function inOrder(currNode) {
+            if(!currNode)  return;
+            if(currNode.left)   inOrder(currNode.left);
+            visited.push(currNode.value);
+            if(currNode.right)  inOrder(currNode.right);
+        }
+        inOrder(this.root);
+        return visited;
+    }
+    DFSPostOrder() {
+        let visited = [];
+        function postOrder(currNode) {
+            if(!currNode)  return;
+            if(currNode.left)   postOrder(currNode.left);
+            if(currNode.right)  postOrder(currNode.right);
+            visited.push(currNode.value);
+        }
+        postOrder(this.root);
+        return visited;
+    }
+    BFS() {
+        if(!this.root) return;
+        let queue = new Queue();
+        let visited = [];
+        queue.enqueue(this.root);
+        while(queue.size > 0) {
+            let node = queue.dequeue();
+            visited.push(node.value);
+            if(node.left)   queue.enqueue(node.left);
+            if(node.right)  queue.enqueue(node.right);  
+        }
+        return visited;
+    }
 }
 
         //   10
         // 5   15
        // 3 6 12 17   
-// insert, find test code
+// DFS preorder, inorder, postorder / BFS test code
 let tree = new BinarySearchTree();
-console.log(tree.find(10)); // undefined;
 tree.insert(10);
 tree.insert(5);
-tree.insert(5); // 중복 생략
 tree.insert(3);
 tree.insert(15);
 tree.insert(12);
 tree.insert(17);
 tree.insert(6);
-console.log(tree.root.left.value);        // 5
-console.log(tree.root.left.left.value);   // 3
-console.log(tree.root.left.right.value);  // 6
-console.log(tree.root.right.value);       // 15
-console.log(tree.root.right.left.value);  // 12
-console.log(tree.root.right.right.value); // 17
-console.log(tree.find(10).value);         // 10
-console.log(tree.find(5).value);          // 5
-console.log(tree.find(17).value);         // 17
-console.log(tree.find(20));               // undefined
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSInOrder());
+console.log(tree.DFSPostOrder());
+console.log(tree.BFS());
+console.log(tree.root); //10
+console.log(tree.root.left);//5
+console.log(tree.root.right);//15
+console.log(tree.root.left.left);//3
+console.log(tree.root.left.right);//6
+console.log(tree.root.right.left);//12
+console.log(tree.root.right.right);//17
+
